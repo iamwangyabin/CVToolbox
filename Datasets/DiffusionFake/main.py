2e6422eaa4af5c861537498e8db737049d11a0e8
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2022-08-30 17:56:32 +0800

    Create main.py

---
+++ Datasets/DiffusionFake/main.py
@@ -0,0 +1,201 @@
+import clip
+import gc
+import numpy as np
+import os
+import pandas as pd
+import requests
+import torch
+import torchvision.transforms as T
+import torchvision.transforms.functional as TF
+
+
+from PIL import Image
+from torch import nn
+from torch.nn import functional as F
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+from models.blip import blip_decoder
+
+from diffusers import StableDiffusionPipeline
+
+
+device_id = 1
+
+device = torch.device('cuda:{}'.format(device_id) if torch.cuda.is_available() else 'cpu')
+
+blip_image_eval_size = 384
+blip_model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model*_base_caption.pth'
+blip_model = blip_decoder(pretrained=blip_model_url, image_size=blip_image_eval_size, vit='base')
+blip_model.eval()
+blip_model = blip_model.to(device)
+
+def generate_caption(pil_image):
+    gpu_image = transforms.Compose([
+        transforms.Resize((blip_image_eval_size, blip_image_eval_size), interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    ])(pil_image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        caption = blip_model.generate(gpu_image, sample=False, num_beams=3, max_length=20, min_length=5)
+    return caption[0]
+
+def load_list(filename):
+    with open(filename, 'r', encoding='utf-8', errors='replace') as f:
+        items = [line.strip() for line in f.readlines()]
+    return items
+
+def rank(model, image_features, text_array, top_count=1):
+    top_count = min(top_count, len(text_array))
+    text_tokens = clip.tokenize([text for text in text_array]).to(device)
+    with torch.no_grad():
+        text_features = model.encode_text(text_tokens).float()
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    similarity = torch.zeros((1, len(text_array))).to(device)
+    for i in range(image_features.shape[0]):
+        similarity += (100.0 * image_features[i].unsqueeze(0) @ text_features.T).softmax(dim=-1)
+    similarity /= image_features.shape[0]
+
+    top_probs, top_labels = similarity.cpu().topk(top_count, dim=-1)
+    return [(text_array[top_labels[0][i].numpy()], (top_probs[0][i].numpy()*100)) for i in range(top_count)]
+
+def interrogate(image, models):
+    caption = generate_caption(image)
+    if len(models) == 0:
+        print(f"\n\n{caption}")
+        return
+
+    table = []
+    bests = [[('',0)]]*5
+    for model_name in models:
+        print(f"Interrogating with {model_name}...")
+        model, preprocess = clip.load(model_name)
+        model.to(device).eval()
+
+        images = preprocess(image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            image_features = model.encode_image(images).float()
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+
+        ranks = [
+            rank(model, image_features, mediums),
+            rank(model, image_features, ["by "+artist for artist in artists]),
+            rank(model, image_features, trending_list),
+            rank(model, image_features, movements),
+            rank(model, image_features, flavors, top_count=3)
+        ]
+
+        for i in range(len(ranks)):
+            confidence_sum = 0
+            for ci in range(len(ranks[i])):
+                confidence_sum += ranks[i][ci][1]
+            if confidence_sum > sum(bests[i][t][1] for t in range(len(bests[i]))):
+                bests[i] = ranks[i]
+
+        row = [model_name]
+        for r in ranks:
+            row.append(', '.join([f"{x[0]} ({x[1]:0.1f}%)" for x in r]))
+
+        table.append(row)
+
+        del model
+        gc.collect()
+    # display(pd.DataFrame(table, columns=["Model", "Medium", "Artist", "Trending", "Movement", "Flavors"]))
+
+    flaves = ', '.join([f"{x[0]}" for x in bests[4]])
+    medium = bests[0][0][0]
+
+    # import pdb;pdb.set_trace()
+    if caption.startswith(medium):
+        return "{} {}, {}, {}, {}".format(caption, bests[1][0][0], bests[2][0][0], bests[3][0][0], flaves)
+    else:
+        return "{}, {} {}, {}, {}, {}".format(caption, medium, bests[1][0][0], bests[2][0][0], bests[3][0][0], flaves)
+        # print(f"\n\n{caption}, {medium} {bests[1][0][0]}, {bests[2][0][0]}, {bests[3][0][0]}, {flaves}")
+
+data_path = "./clip-interrogator/data/"
+
+artists = load_list(os.path.join(data_path, 'artists.txt'))
+flavors = load_list(os.path.join(data_path, 'flavors.txt'))
+mediums = load_list(os.path.join(data_path, 'mediums.txt'))
+movements = load_list(os.path.join(data_path, 'movements.txt'))
+
+sites = ['Artstation', 'behance', 'cg society', 'cgsociety', 'deviantart', 'dribble', 'flickr', 'instagram', 'pexels', 'pinterest', 'pixabay', 'pixiv', 'polycount', 'reddit', 'shutterstock', 'tumblr', 'unsplash', 'zbrush central']
+trending_list = [site for site in sites]
+trending_list.extend(["trending on "+site for site in sites])
+trending_list.extend(["featured on "+site for site in sites])
+trending_list.extend([site+" contest winner" for site in sites])
+
+
+ViTB32 = True #@param{type:"boolean"}
+ViTB16 = True #@param{type:"boolean"}
+ViTL14 = False #@param{type:"boolean"}
+ViTL14_336px = False #@param{type:"boolean"}
+RN101 = False #@param{type:"boolean"}
+RN50 = True #@param{type:"boolean"}
+RN50x4 = False #@param{type:"boolean"}
+RN50x16 = False #@param{type:"boolean"}
+RN50x64 = False #@param{type:"boolean"}
+
+models = []
+if ViTB32: models.append('ViT-B/32')
+if ViTB16: models.append('ViT-B/16')
+if ViTL14: models.append('ViT-L/14')
+if ViTL14_336px: models.append('ViT-L/14@336px')
+if RN101: models.append('RN101')
+if RN50: models.append('RN50')
+if RN50x4: models.append('RN50x4')
+if RN50x16: models.append('RN50x16')
+if RN50x64: models.append('RN50x64')
+
+
+
+# num_inference_steps: Optional[int] = 50,
+# guidance_scale: Optional[float] = 7.5,
+# eta: Optional[float] = 0.0,
+# generator: Optional[torch.Generator] = None,
+# latents: Optional[torch.FloatTensor] = None,
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    use_auth_token=True
+).to(device)
+
+
+# image_path_or_url = "https://cdnb.artstation.com/p/assets/images/images/032/142/769/large/ignacio-bazan-lazcano-book-4-final.jpg" #@param {type:"string"}
+# if str(image_path_or_url).startswith('http://') or str(image_path_or_url).startswith('https://'):
+#     image = Image.open(requests.get(image_path_or_url, stream=True).raw).convert('RGB')
+# else:
+#     image = Image.open(image_path_or_url).convert('RGB')
+
+root = "/data/workspace/datasets/diffusion/"
+
+section = "0000"+str(device_id)
+
+imgname = 0
+for idx, path in enumerate(os.listdir(os.path.join(root, section))):
+    if path.endswith("jpg"):
+        try:
+            image = Image.open(os.path.join(root, section, path)).convert('RGB')
+            #首先get到prompt
+            prompt = interrogate(image, models=models)
+            # 计算图片长宽，应该为8的倍数，进行缩放
+            w, h  = image.size
+            scale = h/w
+            gen_width = 512
+            gen_height = int(gen_width * scale / 64)*64
+            gen_image = pipe(prompt, height=gen_height, width=gen_width, num_inference_steps=100)["sample"][0]
+            gen_image = gen_image.resize((w, h), Image.ANTIALIAS)
+            gen_image.save(os.path.join(root,"fake",str(device_id)+str(imgname).zfill(6)+".jpg"))
+            image.save(os.path.join(root,"real",str(device_id)+str(imgname).zfill(6)+".jpg"))
+            imgname+=1
+
+            f = open(os.path.join(os.path.join(root, section),path[:-3]+"txt"), 'r')
+            caption = f.readlines()[0]
+            f.close()
+
+            f = open(os.path.join(root, "{}.txt".format(device_id)), 'a')
+            f.write(str(device_id)+str(imgname).zfill(6) + "\t｜\t" + prompt + "\t｜\t" + caption + "\n")
+            f.close()
+
+        except:
+            print("error")
